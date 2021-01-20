1aebfdb8044e6c39c95b85736ca6d705397049e6
root <15329885@qq.com>
2021-01-20 12:53:04 +0800

    youhua

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='axpublish',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='',
+    author='yanweijie',
+    author_email='15329885@qq.com',
+    description=''
+)
