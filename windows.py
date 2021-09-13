36615dc47855999d9f619d02f1232064cab52ac1
严伟杰 <weijie.yan@dessmann.com>
2021-09-13 12:57:39 +0800

    默认的

--- windows.py
+++ windows.py
@@ -8,10 +8,11 @@ import time
 
 path = os.environ[config.GVC.HOME] + config.GVC.PATH["HTMLPath"]
 
-
+project=['小嘀师傅','小嘀管家','小嘀进货','原型-ERP']
 
 def click():
-    folder_name = entry.get()
+    folder_name = project[v.get()-1]
+    print(folder_name)
     content = te.get('0.0', 'end')
     uploadfile.compress(path, folder_name)
     api.addlog(content)
@@ -22,9 +23,13 @@ top = tkinter.Tk()
 top.wm_attributes('-topmost', 1)
 top.geometry("400x400+520+250")
 
-entry = tkinter.Entry(top, width=20)
-entry.insert(0, '原型-ERP')
-entry.grid(row=1, column=0)
+
+v=tkinter.IntVar()
+i=1
+for project_name in project:
+    radio=tkinter.Radiobutton(top,variable=v,text=project_name,value=i)
+    radio.grid(row=i, column=0)
+    i=i+1
 
 te = tkinter.Text(
     top,
@@ -33,20 +38,20 @@ te = tkinter.Text(
     borderwidth=1,
 )
 te.insert(index=tkinter.END, chars='更新日志：')
-te.grid(row=0, column=0)
+te.grid(row=i+2, column=0)
 
 B = tkinter.Button(
     top,
     text="发布",
     command=click
-).grid(row=3, column=0)
+).grid(row=i, column=0)
 
 L = tkinter.Text(
     top,
     background='#E0E0E0',
     height=2,
     width=10
-).grid(row=4, column=0)
+).grid(row=i+1, column=0)
 
 if __name__ == '__main__':
     top.mainloop()
